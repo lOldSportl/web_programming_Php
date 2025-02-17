7eba941fb8625cecc6ed9e3f40d1dca4bb9618b1
lOldSportl <mconor393@gmail.com>
2025-02-17 20:14:47 +0300

    Authorization form

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "DB";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn)
+{
+    die("Ошибка подключения");
+}
+else
+{
+    
+}
+?>
